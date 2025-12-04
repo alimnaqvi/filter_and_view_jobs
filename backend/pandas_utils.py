403fffbee456783255690189f5c0c276a447dc97
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-12-04 14:23:07 +0100

    improve your filtering logic and prevent duplicates

--- backend/pandas_utils.py
+++ backend/pandas_utils.py
@@ -11,47 +11,53 @@ def apply_filters_from_params(df: pd.DataFrame, request: Request):
     seniority: list = request.query_params.getlist("seniority")
     if seniority and "all" not in seniority:
         seniority_lower = df['Role seniority'].fillna('N/A').str.lower().str
+        
+        combined_mask = pd.Series([False] * len(df), index=df.index)
+
         internship_mask = (seniority_lower.contains("intern")) | (seniority_lower.contains("praktik"))
         entry_mask = seniority_lower.contains("entry")
         junior_mask = seniority_lower.contains("junior")
         mid_mask = (seniority_lower.contains("mid")) | (seniority_lower.contains("medi"))
         senior_mask = seniority_lower.contains("senior")
         unclear_mask = (seniority_lower.contains("unclear")) | (seniority_lower.contains("multiple"))
-        filter_df = pd.DataFrame()
+
         if "internship" in seniority:
-            filter_df = pd.concat([filter_df, df[internship_mask]])
+            combined_mask |= internship_mask
         if "entry" in seniority:
-            filter_df = pd.concat([filter_df, df[entry_mask]])
+            combined_mask |= entry_mask
         if "junior" in seniority:
-            filter_df = pd.concat([filter_df, df[junior_mask]])
+            combined_mask |= junior_mask
         if "mid" in seniority:
-            filter_df = pd.concat([filter_df, df[mid_mask]])
+            combined_mask |= mid_mask
         if "senior" in seniority:
-            filter_df = pd.concat([filter_df, df[senior_mask]])
+            combined_mask |= senior_mask
         if "unclear" in seniority:
-            filter_df = pd.concat([filter_df, df[unclear_mask]])
+            combined_mask |= unclear_mask
         if "other" in seniority:
-            filter_df = pd.concat([filter_df, df[~internship_mask & ~entry_mask & ~entry_mask & ~junior_mask & ~mid_mask & ~senior_mask & ~unclear_mask]])
-        # finally modify the original df with applied filters
-        df = filter_df
+            other_mask = ~(internship_mask | entry_mask | junior_mask | mid_mask | senior_mask | unclear_mask)
+            combined_mask |= other_mask
+        
+        df = df[combined_mask]
 
     # Apply german filter
     german = request.query_params.getlist("german")
     if german and "all" not in german:
-        # df['German language fluency required'] = df['German language fluency required'].fillna('N/A')
-        filter_df = pd.DataFrame()
+        german_lower = df['German language fluency required'].fillna('N/A').str.lower()
+        
+        combined_mask = pd.Series([False] * len(df), index=df.index)
+
         if "intermediate" in german:
-            filter_df = pd.concat([filter_df, df[df['German language fluency required'].str.lower().str.contains("intermediate")]])
+            combined_mask |= german_lower.str.contains("intermediate")
         if "yes" in german:
-            filter_df = pd.concat([filter_df, df[df['German language fluency required'].str.contains("Yes")]])
+            combined_mask |= german_lower.str.startswith("yes")
         if "no" in german:
-            filter_df = pd.concat([filter_df, df[df['German language fluency required'].str.contains("No")]])
+            combined_mask |= german_lower.str.startswith("no")
         if "other" in german:
-            filter_df = pd.concat([filter_df, df[
-                ~df['German language fluency required'].str.startswith("Yes") &
-                ~df['German language fluency required'].str.startswith("No")
-            ]])
-        # finally modify the original df with applied filters
-        df = filter_df
+            other_mask = ~(german_lower.str.startswith("yes") | german_lower.str.startswith("no") | german_lower.str.contains("intermediate"))
+            combined_mask |= other_mask
+        
+        df = df[combined_mask]
+
+    df = df.drop_duplicates().reset_index(drop=True)
 
     return df
